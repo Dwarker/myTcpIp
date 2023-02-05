0ca88e34543eb4b11a0c6eb14761d4daf9c8745c
wangxuan <961767849@qq.com>
2023-02-05 22:05:08 +0800

    抓取虚拟机发到本地的ping命令的数据,并修改前两个字节直接扔回网卡

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -1,12 +1,10 @@
 ﻿/**
  * @file main.c
- * @author lishutong (527676163@qq.com)
+ * @author wangxuan
  * @brief 测试主程序，完成一些简单的测试主程序
  * @version 0.1
- * @date 2022-10-23
+ * @date 2023-02-05
  *
- * @copyright Copyright (c) 2022
- * @note 该源码配套相应的视频课程，请见源码仓库下面的README.md
  */
 #include <stdio.h>
 #include <sys_plat.h>
@@ -17,20 +15,33 @@ int main (void) {
 	while (pcap) {
 		static uint8_t buffer[1024];
 		static int counter = 0;
+		//接收的数据包包头地址
+		struct pcap_pkthdr *pkthdr;
+		//从网卡获取的数据地址
+		const uint8_t *pkt_data;
 
 		plat_printf("begin test: %d\n", counter++);
 		for (int i = 0; i < sizeof(buffer); i++) {
 			buffer[i] = i;
 		}
 
+		//接收数据:如果没有数据, 会等待,所以该循环不需要利用sleep
+		//达到避免过渡消耗CPU的作用
+		if (pcap_next_ex(pcap, &pkthdr, &pkt_data) != 1) {
+			continue;
+		}
+
+		//拷贝获取数据
+		int len = pkthdr->len > sizeof(buffer) ? sizeof(buffer) : pkthdr->len;
+		plat_memcpy(buffer, pkt_data, len);
+		buffer[0] = 3;
+		buffer[1] = 4;
+
 		//发送数据给网卡
 		if (pcap_inject(pcap, buffer, sizeof(buffer)) == -1) {
 			plat_printf("pcap send: send packet failed %s\n", pcap_geterr(pcap));
 			break;
 		}
-
-		//防止过渡消耗CPU, 导致影响其他程序的执行
-		sys_sleep(10);
 	}
 
 	printf("Hello, world");
