8484544121d67f5a9976a8e2f18a6efc0442701e
wangxuan <961767849@qq.com>
2023-02-10 21:30:16 +0800

    链表相关操作及测试

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -13,6 +13,7 @@
 #include "echo/tcp_echo_server.h"
 #include "net.h"
 #include "dbg.h"
+#include "nlist.h"
 
 static sys_sem_t sem;
 static sys_mutex_t mutex;
@@ -64,6 +65,70 @@ net_err_t netdev_init (void) {
 	return NET_ERR_OK;
 }
 
+//基础组件--链表测试
+typedef struct _tnode_t {
+	int id;//测试方便观察
+	nlist_node_t node;
+}tnode_t;
+
+void nlist_test(void) {
+	#define NODE_CNT 4
+	tnode_t node[NODE_CNT];
+	nlist_t list;
+
+	nlist_init(&list);
+	for (int i = 0; i < NODE_CNT; i++) {
+		node[i].id = i;
+		nlist_insert_fist(&list, &node[i].node);
+	}
+
+	plat_printf("insert first\n");
+
+	nlist_node_t *p;
+	nlist_for_each(p, &list) {
+		tnode_t *tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id:%d\n", tnode->id);
+	}
+
+	plat_printf("remove first\n");
+	for (int i = 0; i < NODE_CNT; i++) {
+		p = nlist_remove_first(&list);
+		plat_printf("id:%d\n", nlist_entry(p, tnode_t, node)->id);
+	}
+
+	for (int i = 0; i < NODE_CNT; i++) {
+		node[i].id = i;
+		nlist_insert_last(&list, &node[i].node);
+	}
+
+	plat_printf("insert last\n");
+	nlist_for_each(p, &list) {
+		tnode_t *tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id:%d\n", tnode->id);
+	}
+
+	plat_printf("remove last\n");
+	for (int i = 0; i < NODE_CNT; i++) {
+		p = nlist_remove_last(&list);
+		plat_printf("id:%d\n", nlist_entry(p, tnode_t, node)->id);
+	}
+
+	plat_printf("insert after\n");
+	for (int i = 0; i < NODE_CNT; i++) {
+		node[i].id = i;
+		nlist_insert_after(&list, nlist_first(&list), &node[i].node);
+	}
+
+	nlist_for_each(p, &list) {
+		tnode_t *tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id:%d\n", tnode->id);
+	}
+}
+
+void basic_test(void) {
+	nlist_test();
+}
+
 #define DBG_TEST DBG_LEVEL_INFO
 
 int main (void) {
@@ -71,12 +136,14 @@ int main (void) {
 	dbg_warning(DBG_TEST, "warning");
 	dbg_error(DBG_TEST, "error");
 
-	dbg_assert(1 == 1, "failed");
-	dbg_assert(1 == 0, "failed");
+	//dbg_assert(1 == 1, "failed");
+	//dbg_assert(1 == 0, "failed");
 
 	net_init();
-	net_start();
 
+	basic_test();
+
+	net_start();
 	netdev_init();
 
 	return 0;
