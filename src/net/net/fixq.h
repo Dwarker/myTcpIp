db624645d91489abeb1d068a173adf0b22f35126
wangxuan <961767849@qq.com>
2023-02-11 17:20:12 +0800

    定义消息队列

---
+++ src/net/net/fixq.h
@@ -0,0 +1,19 @@
+#ifndef FIXQ_H
+#define FIXQ_H
+
+#include "nlocker.h"
+#include "sys.h"
+
+typedef struct _fixq_t {
+    int size;   //可存放的消息数
+    int in, out, cnt;//可写和可读的位置,当前有多少个消息
+    void **buf;
+
+    nlocker_t locker;
+    sys_sem_t recv_sem;
+    sys_sem_t send_sem;
+}fixq_t;
+
+net_err_t fixq_init (fixq_t *q, void **buf, int size, nlocker_type_t type);
+
+#endif
