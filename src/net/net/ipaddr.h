bf30c34e92bd169e105007cc937b8f88fd9539fc
wangxuan <961767849@qq.com>
2023-02-18 18:13:28 +0800

    网络地址转换接口

--- src/net/net/ipaddr.h
+++ src/net/net/ipaddr.h
@@ -2,6 +2,7 @@
 #define  IPADDR_H
 
 #include <stdint.h>
+#include "net_err.h"
 
 #define IPV4_ADDR_SIZE  4
 
@@ -18,5 +19,6 @@ typedef struct _ipaddr_t {
 
 //初始化时,填入缺省ip值
 void ipaddr_set_any (ipaddr_t *ip);
+net_err_t ipaddr_from_str (ipaddr_t *dest, const char *str);
 
 #endif
--- src/net/src/ipaddr.c
+++ src/net/src/ipaddr.c
@@ -4,3 +4,26 @@ void ipaddr_set_any (ipaddr_t *ip) {
     ip->type = IPADDR_V4;
     ip->q_addr = 0;
 }
+
+net_err_t ipaddr_from_str (ipaddr_t *dest, const char *str) {
+    if (!dest || !str) {
+        return NET_ERR_PARAM;
+    }
+
+    uint8_t *p = dest->a_addr;
+    uint8_t sub_addr = 0;
+    char c;
+    while ((c = *str++) != '\0') {
+        if ((c >= '0') && (c <= '9')) {
+            sub_addr = sub_addr * 10 + (c - '0');
+        } else if (c == '.') {
+            *p++ = sub_addr;
+            sub_addr = 0;
+        } else {
+            return NET_ERR_PARAM;
+        }
+    }
+
+    *p++ = sub_addr;
+    return NET_ERR_OK;
+}
