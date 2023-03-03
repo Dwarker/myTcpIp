a8110dc456b3484093fca04668e14d221cdb59cb
wangxuan <961767849@qq.com>
2023-03-03 20:30:54 +0800

    检查包头正确性

--- src/net/src/ipv4.c
+++ src/net/src/ipv4.c
@@ -30,6 +30,14 @@ static net_err_t is_pkt_pk(ipv4_pkt_t *pkt, int size, netif_t *netif) {
         return NET_ERR_SIZE;
     }
 
+    if (pkt->hdr.hdr_checksum) {
+        uint16_t c = checksum16(pkt, hdr_len, 0, 1);
+        if (c != 0) {
+            dbg_warning(DBG_IP, "bad checksum");
+            return NET_ERR_BROKEN;
+        }
+    }
+
     return NET_ERR_OK;
 }
 
