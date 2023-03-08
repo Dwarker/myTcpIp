c35dcda3a572a62b05fe8671d2a8b582b8367db1
wangxuan <961767849@qq.com>
2023-03-08 13:40:28 +0800

    IP数据报的分片重组

--- src/net/src/ipv4.c
+++ src/net/src/ipv4.c
@@ -170,6 +170,68 @@ static net_err_t frag_insert(ip_frag_t *frag, pktbuf_t *buf, ipv4_pkt_t *pkt) {
     return NET_ERR_OK;
 }
 
+static int frag_is_all_arrived(ip_frag_t *frag) {
+    int offset = 0;
+
+    ipv4_pkt_t *pkt = (ipv4_pkt_t *)0;
+    nlist_node_t *node;
+    nlist_for_each(node, &frag->buf_list) {
+        pktbuf_t *buf = nlist_entry(node, pktbuf_t, node);
+        pkt = (ipv4_pkt_t *)pktbuf_data(buf);
+
+        int curr_offset = get_frag_start(pkt);
+        if (curr_offset != offset) {
+            //说明中间还有分片没到达
+            return 0;
+        }
+
+        offset += get_data_size(pkt);
+    }
+
+    //判断最后一个分片的more字段
+    return pkt ? !pkt->hdr.more : 0;
+}
+
+//合并分片
+static pktbuf_t *frag_join(ip_frag_t *frag) {
+    pktbuf_t *target = (pktbuf_t *)0;
+    nlist_node_t *node;
+
+    while ((node = nlist_remove_first(&frag->buf_list))) {
+        pktbuf_t *curr = nlist_entry(node, pktbuf_t, node);
+
+        if (!target) {
+            target = curr;
+            continue;
+        }
+
+        ipv4_pkt_t *pkt = (ipv4_pkt_t *)pktbuf_data(curr);
+        net_err_t err = pktbuf_remove_header(curr, ipv4_hdr_size(pkt));
+        if (err < 0) {
+            dbg_error(DBG_IP, "remove hdr failed.");
+            pktbuf_free(curr);
+            goto free_and_return;
+        }
+
+        //合并
+        err = pktbuf_join(target, curr);
+        if (err < 0) {
+            dbg_error(DBG_IP, "join ip frag failed.");
+            pktbuf_free(curr);
+            goto free_and_return;
+        }
+    }
+    frag_free(frag);
+    return target;
+free_and_return:
+    if (target) {
+        pktbuf_free(target);
+    }
+    //失败的情况,直接释放对应的分片头及后面的分片
+    frag_free(frag);
+    return (pktbuf_t *)0;
+}
+
 net_err_t ipv4_init(void) {
     dbg_info(DBG_IP, "init ip\n");
 
@@ -225,26 +287,6 @@ static void iphdr_htons(ipv4_pkt_t *pkt) {
     pkt->hdr.frag_all = x_htons(pkt->hdr.frag_all);
 }
 
-static net_err_t ip_frag_in(netif_t *netif, pktbuf_t *buf, ipaddr_t *src_ip, ipaddr_t *dest_ip) {
-    ipv4_pkt_t *curr = (ipv4_pkt_t *)pktbuf_data(buf);
-
-    //同一个数据包,hdr.id不变
-    ip_frag_t *frag = frag_find(src_ip, curr->hdr.id);
-    if (!frag) {
-        frag = frag_alloc();
-        frag_add(frag, src_ip, curr->hdr.id);
-    }
-
-    net_err_t err = frag_insert(frag, buf, curr);
-    if (err < 0) {
-        dbg_warning(DBG_IP, "frag insert failed.");
-        return err;
-    }
-
-    display_ip_frags();
-    return NET_ERR_OK;
-}
-
 static net_err_t ip_normal_in(netif_t *netif, pktbuf_t *buf, ipaddr_t *src_ip, ipaddr_t *dest_ip) {
     ipv4_pkt_t *pkt = (ipv4_pkt_t *)pktbuf_data(buf);
 
@@ -274,6 +316,46 @@ static net_err_t ip_normal_in(netif_t *netif, pktbuf_t *buf, ipaddr_t *src_ip, i
     }
 
     return NET_ERR_UNREACH;
+}
+
+static net_err_t ip_frag_in(netif_t *netif, pktbuf_t *buf, ipaddr_t *src_ip, ipaddr_t *dest_ip) {
+    ipv4_pkt_t *curr = (ipv4_pkt_t *)pktbuf_data(buf);
+
+    //同一个数据包,hdr.id不变
+    ip_frag_t *frag = frag_find(src_ip, curr->hdr.id);
+    if (!frag) {
+        frag = frag_alloc();
+        frag_add(frag, src_ip, curr->hdr.id);
+    }
+
+    net_err_t err = frag_insert(frag, buf, curr);
+    if (err < 0) {
+        dbg_warning(DBG_IP, "frag insert failed.");
+        return err;
+    }
+
+    //判断是否合并
+    if (frag_is_all_arrived(frag)) {
+        pktbuf_t *full_buf = frag_join(frag);
+        if (!full_buf) {
+            //合并失败直接返回正确,不然会在外层释放分片内存,
+            //但是在合并的时候,已经释放,这里防止重复释放
+            dbg_error(DBG_IP, "join ip bufs failed.");
+            display_ip_frags();
+            return NET_ERR_OK;
+        }
+
+        //传给其他模块
+        err = ip_normal_in(netif, full_buf, src_ip, dest_ip);
+        if (err < 0) {
+            dbg_warning(DBG_IP, "ip frag in failed.", err);
+            pktbuf_free(full_buf);
+            return NET_ERR_OK;
+        }
+    }
+
+    display_ip_frags();
+    return NET_ERR_OK;
 }
 
 net_err_t ipv4_in(netif_t *netif, pktbuf_t *buf) {
